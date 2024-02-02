57856275dbd919d1c8c77bdb3ce06e48314d0bdf
Victor Madera <v.maderaalbors@gmail.com>
2024-02-02 13:30:01 +0000

    updated script.sh and RREADME.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# V.MaderaAlborsDevOps
+# Week 2 Lab - Bash & Git
+# Victor Madera Albors x22223487
+https://github.com/VMaderaAlbors/V.MaderaAlborsDevOps
+---
+To run the script.sh run: 'bash script.sh'
+
--- script.sh
+++ script.sh
@@ -9,4 +9,15 @@ echo "creating files"
 touch fol_1/1_1.txt
 touch fol_1/1_2.txt
 touch fol_1/1_3.txt
-echo ""
+touch fol_2/2_1.txt
+touch fol_2/2_2.txt
+touch fol_2/2_3.txt
+echo "creating permissions"
+cd fol_1
+chmod 600 *_1.txt
+chmod 600 *_3.txt
+cd ..
+cd fol_2
+chmod 777 *_2.txt
+cd ..
+
